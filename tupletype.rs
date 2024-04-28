703005dc3b72f76327c38176fc0f3394fabb03ab
James-wasonga <jameswasonga22@gmail.com>
2024-04-28 22:17:31 +0300

    data types in Rust

--- tupletype.rs
+++ tupletype.rs
@@ -5,5 +5,7 @@ fn main(){
     let(_x, _y, _z) = tup;
     println!("The value of x is: {_x}");
 
-    
+    //accessing tupple using period[.] and index
+    let _ans = tup.1;
+    println!("The value of y is : {_y}"); 
 }

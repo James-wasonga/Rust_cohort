b42ecc131fb45df6a04f628583f1dc0fd7511d41
James-wasonga <jameswasonga22@gmail.com>
2024-05-03 15:34:06 +0300

    if condition

---
+++ if.rs
@@ -0,0 +1,14 @@
+fn main(){
+//using  the bool to make confirmation of the writings 
+    let first : bool = true;
+    let second : bool = false;
+
+ let month = 12;
+
+if month > 20 {
+    println!("The month is{second}");
+}else{
+    println!("The month is:{first}");
+}
+
+}
